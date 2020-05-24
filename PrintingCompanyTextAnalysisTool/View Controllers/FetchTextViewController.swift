de4f3f11d4c80923397f27e550e11eb59a44e607
hectorsvill <hectorstevenvillasano@gmail.com>
2020-05-24 14:55:41 -0700

    implement  UIDocumentPicker

--- PrintingCompanyTextAnalysisTool/View Controllers/FetchTextViewController.swift
+++ PrintingCompanyTextAnalysisTool/View Controllers/FetchTextViewController.swift
@@ -10,21 +10,71 @@ import UIKit
 
 class FetchTextViewController: UIViewController {
 
+    @IBOutlet weak var newInputButtonFileButton: UIButton!
+    @IBOutlet weak var tableView: UITableView!
+
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        configureViews()
     }
-    
 
-    /*
-    // MARK: - Navigation
+    private func configureViews() {
+        newInputButtonFileButton.layer.cornerRadius = 17
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
     }
-    */
+
+    @IBAction func newInputButtonFileButtonPressed(_ sender: Any) {
+//        let types = ["kUTTypePDF", "kUTTypeText", "kUTTypeRTF", "kUTTypeSpreadsheet"]
+        let documentPicker = UIDocumentPickerViewController(documentTypes: ["public.data"], in: .import)
+        documentPicker.delegate = self
+        documentPicker.allowsMultipleSelection = false
+
+        present(documentPicker, animated: true, completion: nil)
+
+    }
+
+}
+
+extension FetchTextViewController: UIDocumentPickerDelegate, UINavigationControllerDelegate {
+    func documentPickerWasCancelled(_ controller: UIDocumentPickerViewController) {
+        dismiss(animated: true)
+    }
+
+    func documentPicker(_ controller: UIDocumentPickerViewController, didPickDocumentsAt urls: [URL]) {
+
+        guard let url = urls.first else { return }
+
+        do {
+            let data = try Data(contentsOf: url, options: .uncached)
+            print(data)
+        } catch {
+            NSLog("$@", error.localizedDescription)
+        }
+
+    }
+}
+
+extension FetchTextViewController: UITableViewDelegate, UITableViewDataSource {
+
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 3
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return "header \(section)"
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "FileCell", for: indexPath)
+        cell.textLabel?.text = "text label"
+        cell.detailTextLabel?.text = "Detail Label"
+        return cell
+    }
 
 }
