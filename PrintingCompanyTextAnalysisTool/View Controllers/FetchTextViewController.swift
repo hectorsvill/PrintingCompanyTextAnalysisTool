bc40739b587ffd88e26c75662f54910ec12a6af8
hectorsvill <hectorstevenvillasano@gmail.com>
2020-05-24 13:57:21 -0700

    init

---
+++ PrintingCompanyTextAnalysisTool/View Controllers/FetchTextViewController.swift
@@ -0,0 +1,30 @@
+//
+//  FetchTextViewController.swift
+//  PrintingCompanyTextAnalysisTool
+//
+//  Created by Hector S. Villasano on 5/24/20.
+//  Copyright © 2020 Hector Steven  Villasano. All rights reserved.
+//
+
+import UIKit
+
+class FetchTextViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
