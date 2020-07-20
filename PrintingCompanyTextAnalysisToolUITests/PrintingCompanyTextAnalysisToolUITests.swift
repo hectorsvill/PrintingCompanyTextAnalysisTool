5960d9b2a0db500020462614157ea1c4d6b96e49
hectorsvill <hectorstevenvillasano@gmail.com>
2020-07-20 14:49:06 -0700

    ui test: View hierarchy with debugdescription

--- PrintingCompanyTextAnalysisToolUITests/PrintingCompanyTextAnalysisToolUITests.swift
+++ PrintingCompanyTextAnalysisToolUITests/PrintingCompanyTextAnalysisToolUITests.swift
@@ -9,27 +9,25 @@
 import XCTest
 
 class PrintingCompanyTextAnalysisToolUITests: XCTestCase {
+    let app = XCUIApplication()
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
+    
+    override func setUp() {
         continueAfterFailure = false
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // UI tests must launch the application that they test.
-        let app = XCUIApplication()
         app.launch()
-
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    override func tearDown() {
+        
+    }
+    
+    func testNewInputFile() {
+//        print(app.debugDescription)
+        let newInputFileButton = app.buttons["NewInputFileButton"]
+        XCTAssert(newInputFileButton.isHittable)
+        newInputFileButton.tap()
+        
+        
     }
 
     func testLaunchPerformance() throws {
